3c06555a29345f33903e567a250ea366fbd71ae7
Zach T <ztrokey@gmail.com>
2021-07-06 19:43:51 -0600

    add registration functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :dashboard, only: [:index, :create]
 end
---
+++ spec/features/users/registration_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "User registration form" do
+  it "creates new user" do
+    visit new_user_path
+
+    username = 'chris_p_bacon@gmail.com'
+    password = 'test123'
+
+    fill_in 'user[email]', with: username
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+
+
+    click_on 'Register'
+    expect(page).to have_content("Welcome #{username}!")
+    expect(current_path).to eq(dashboard_index_path)
+  end
+end
