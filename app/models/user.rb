9dcb7f28205c28b7b85a95860b2002c43e401ba7
Joe Ray <jray100.jr@gmail.com>
2021-07-06 19:03:37 -0600

    Update user model w/ relationships and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
+  validates :user_name, :email, :password_digest, presence: true
+  validates :user_name, :email, uniqueness: true
+  validates_format_of :email, with: /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
+  # /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  has_secure_password
 end
