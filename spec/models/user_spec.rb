9bc89636bfeb5e1208a46420e889ea30d6dae738
Joe Ray <jray100.jr@gmail.com>
2021-07-06 19:02:57 -0600

    Create model test for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'relationships' do
+    it { should have_many(:friendships) }
+    it { should have_many(:friends).through(:friendships) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:user_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_uniqueness_of(:user_name) }
+    it { should validate_uniqueness_of(:email) }
+    it { should have_secure_password }
+  end
+end
