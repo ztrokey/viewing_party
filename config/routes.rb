8f4febdc230c664b9a9c725e88a898b95f02fb63
Joe Ray <jray100.jr@gmail.com>
2021-07-07 16:46:05 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
+  get '/register', to: 'users#new'
+  post '/users', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :dashboard, only: [:index, :create]
 end
