20e0459ff79dfc7a04cd43e05c581da7ff129675
Zach T <ztrokey@gmail.com>
2021-07-08 11:18:15 -0600

    add association between user and party

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   describe 'relationships' do
     it { should have_many(:friendships) }
     it { should have_many(:friends).through(:friendships) }
-    # it { should have_many(:parties) } #need to discuss
+    it { should have_many(:parties) } 
   end
 
   describe 'validations' do
