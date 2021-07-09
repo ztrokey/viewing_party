7f064cc73b62f76f1029a799c2c446507977214d
Zach T <ztrokey@gmail.com>
2021-07-09 17:15:18 -0600

    create starting point for movie api consumption

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,14 @@
 class MoviesController < ApplicationController
-before_action :authenticate_user
+  before_action :authenticate_user
 
   def index
     conn = Faraday.new(
-      url: "https://api.themoviedb.org",
-      params: { api_key: ENV['movie_db_api'] })
+      url: 'https://api.themoviedb.org',
+      params: { api_key: ENV['movie_db_api'] }
+    )
     response = conn.get("/3/search/movie?&query=#{params[:search]}")
     movies = JSON.parse(response.body, symbolize_name: true)
-    require 'pry'; binding.pry
   end
 
-  def show
-  end
+  def show; end
 end
