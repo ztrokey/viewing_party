792b43225088ebedd2038e6bda9c77f48bcc344e
Joe Ray <jray100.jr@gmail.com>
2021-07-10 13:36:16 -0600

    Update movies controller with conditional

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,27 +1,27 @@
 class MoviesController < ApplicationController
     def index
-      conn = Faraday.new(
-      url: "https://api.themoviedb.org",
-      params: { api_key: ENV['movie_db_api'] })
+      if !params[:search]
+        conn = Faraday.new(
+        url: "https://api.themoviedb.org",
+        params: { api_key: ENV['movie_db_api'] })
+        page1 = conn.get('/3/movie/top_rated?&page=1')
+        page2 = conn.get('/3/movie/top_rated?&page=2')
 
-      page1 = conn.get('/3/movie/top_rated?&page=1')
-      page2 = conn.get('/3/movie/top_rated?&page=2')
+        parsed1 = JSON.parse(page1.body, symbolize_names: true)
+        parsed2 = JSON.parse(page2.body, symbolize_names: true)
 
-      parsed1 = JSON.parse(page1.body, symbolize_names: true)
-      parsed2 = JSON.parse(page2.body, symbolize_names: true)
-
-      @movies = parsed1[:results] + parsed2[:results]
-
-      # movie search api below
-      conn = Faraday.new(
-        url: 'https://api.themoviedb.org',
-        params: { api_key: ENV['movie_db_api'] }
-      )
-      response1 = conn.get("/3/search/movie?&query=#{params[:search]}&page=1")
-      response2 = conn.get("/3/search/movie?&query=#{params[:search]}&page=2")
-      parsed1 = JSON.parse(response1.body, symbolize_names: true)
-      parsed2 = JSON.parse(response2.body, symbolize_names: true)
-      @results = parsed1[:results] + parsed2[:results]
+        @movies = parsed1[:results] + parsed2[:results]
+      elsif params[:search] 
+        conn = Faraday.new(
+        url: "https://api.themoviedb.org",
+        params: { api_key: ENV['movie_db_api'] })
+        response_searched1 = conn.get("/3/search/movie?&query=#{params[:search]}&page=1")
+        response_searched2 = conn.get("/3/search/movie?&query=#{params[:search]}&page=2")
+        parsed_searched1 = JSON.parse(response_searched1.body, symbolize_names: true)
+        parsed_searched2 = JSON.parse(response_searched2.body, symbolize_names: true)
+        # require 'pry'; binding.pry
+        @results = parsed_searched1[:results] + parsed_searched2[:results]
+      end
     end
 
     def show; end
