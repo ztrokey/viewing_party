bd54b34d8e9dcdcd66bd33545f8e7628669b4aa6
Joe Ray <jray100.jr@gmail.com>
2021-07-12 16:26:42 -0600

    Update methods in movie facade

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -3,22 +3,45 @@ class MoviesFacade
     def top_rated_movies
       movies = MoviesService.get_top_rated_movies
       movies.map do |movie|
-        Movie.new(movie)
+        movie = MoviesService.get_movie_details(movie[:id])
+        cast = MoviesService.get_movie_cast(movie[:id])
+        reviews = MoviesService.get_movie_reviews(movie[:id])
+        Movie.new(movie, cast, reviews)
       end
     end
 
     def search_result_movies(search_params)
       movies = MoviesService.get_search_results(search_params)
       movies.map do |movie|
-        Movie.new(movie)
+        movie = MoviesService.get_movie_details(movie[:id])
+        cast = MoviesService.get_movie_cast(movie[:id])
+        reviews = MoviesService.get_movie_reviews(movie[:id])
+        Movie.new(movie, cast, reviews)
       end
     end
 
     def get_movie_details(movie_id)
       movie = MoviesService.get_movie_details(movie_id)
-      Movie.new(movie)
+      cast = MoviesService.get_movie_cast(movie_id)
+      reviews = MoviesService.get_movie_reviews(movie_id)
+      Movie.new(movie, cast, reviews)
+    end
+
+    def get_movie_credits(movie_id)
+      movie = MoviesService.get_movie_details(movie_id)
+      cast = MoviesService.get_movie_cast(movie_id)
+      reviews = MoviesService.get_movie_reviews(movie_id)
+      Movie.new(movie, cast, reviews)
+    end
+
+    def get_movie_reviews(movie_id)
+      movie = MoviesService.get_movie_details(movie_id)
+      cast = MoviesService.get_movie_cast(movie_id)
+      reviews = MoviesService.get_movie_reviews(movie_id)
+      Movie.new(movie, cast, reviews)
     end
   end
 end
+
 
 # need to add cast and reviews either to Movies PORO or create new PORO's
