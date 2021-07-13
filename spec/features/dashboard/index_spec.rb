88822f25711b00d80b7cea8b50310e469842d7cc
Zach T <ztrokey@gmail.com>
2021-07-12 21:02:08 -0600

    add tests for adding friends

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'dashboard index page' do
-  it 'requires a user to be logged in' do
+  it 'redirects to the root_path if a user is not logged in' do
+    user1 = User.create(user_name: 'Bob Barker', email: 'spinthat.wheel@example.com', password: 'sploot')
+
     visit dashboard_index_path
 
     expect(current_path).to eq(root_path)
-
+  end
+  it 'allows a logged in user to visit the dashboard page' do
     visit root_path
 
     user1 = User.create(user_name: 'Bob Barker', email: 'spinthat.wheel@example.com', password: 'sploot')
@@ -25,17 +28,65 @@ RSpec.describe 'dashboard index page' do
 
     click_button 'Log in'
 
-    visit dashboard_index_path
-
+    expect(current_path).to eq(dashboard_index_path)
     expect(page).to have_content("Welcome #{user1.user_name}!")
-
     expect(page).to have_selector(:link_or_button, 'Discover Movies')
-
     within('#friends') do
       expect(page).to have_content(user2.user_name)
       expect(page).to have_content(user3.user_name)
     end
-
     expect(page).to have_content("Your Viewing Parties")
+  end
+  it 'has a section to add friends' do
+    visit root_path
+
+    user1 = User.create(user_name: 'test1', email: 'test1@example.com', password: 'sploot')
+    user2 = User.create(user_name: 'test2', email: 'test2@example.com', password: 'password123')
+    user3 = User.create(user_name: 'test3', email: 'test3@example.com', password: '123password')
+
+    click_button 'Log in'
+
+    fill_in :user_name, with: 'test1'
+    fill_in :email, with: 'test1@example.com'
+    fill_in :password, with: 'sploot'
+
+    click_button 'Log in'
+
+    within('#friends') do
+      expect(page).to have_content('You currently have no friends')
+      expect(page).to have_button('Add Friend')
+    end
+  end
+  it 'can add a friend' do
+    visit root_path
+
+    user1 = User.create(user_name: 'test1', email: 'test1@example.com', password: 'sploot')
+    user2 = User.create(user_name: 'test2', email: 'test2@example.com', password: 'password123')
+    user3 = User.create(user_name: 'test3', email: 'test3@example.com', password: '123password')
+
+    click_button 'Log in'
+
+    fill_in :user_name, with: 'test1'
+    fill_in :email, with: 'test1@example.com'
+    fill_in :password, with: 'sploot'
+
+    click_button 'Log in'
+
+    within('#friends') do
+      fill_in "friends",	with: "#{user2.email}"
+      click_button 'Add Friend'
+
+      fill_in "friends",	with: "#{user3.email}"
+      click_button 'Add Friend'
+
+      expect(current_path).to eq(dashboard_index_path)
+      expect(page).to have_content("#{user2.user_name}")
+      expect(page).to have_content("#{user3.user_name}")
+    end
+    within('#friends') do
+      fill_in "friends",	with: 'idontexist@example.com'
+      click_button 'Add Friend'
+    end
+    expect(page).to have_content('Please enter a vaild email address')
   end
 end
