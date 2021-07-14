0bb9eef085677b98a96c1eb8d7418deaa54140c6
Joe Ray <jray100.jr@gmail.com>
2021-07-13 18:42:11 -0600

    Update parties migration

--- db/migrate/20210707000738_create_parties.rb
+++ db/migrate/20210707000738_create_parties.rb
@@ -2,7 +2,11 @@ class CreateParties < ActiveRecord::Migration[5.2]
   def change
     create_table :parties do |t|
       t.references :host, foreign_key: { to_table: :users }
-      t.integer :movie_api
+      t.string :movie_title
+      t.date :start_date
+      t.time :start_time
+      t.integer :length
+      t.integer :movie_id
 
       t.timestamps
     end
